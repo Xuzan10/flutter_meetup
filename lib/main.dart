bd1f067a79a63aa5b7206e489e7f42d9f2a11e4c
Xuzan <moonlightsujan@gmail.com>
2019-03-21 11:16:54 +0545

    * live coding

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,86 @@
 
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
-import 'package:flutter_meetup/category_route.dart';
 
 // TODO: Import the CategoryRoute widget
 
 /// The function that is called when main.dart is run.
 void main() {
-  runApp(UnitConverterApp());
+  runApp(MyApp());
 }
 
-/// This widget is the root of our application.
-///
-/// The first screen we see is a list [Categories].
-class UnitConverterApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  Widget getBox(Color color) {
+    return Container(
+      height: 200,
+      width: 300,
+      color: color,
+    );
+  }
+
+  Widget getListTile(String title, IconData icon) {
+    return ListTile(
+      title: Text(title),
+      leading: Icon(icon),
+      onTap: () {
+        print("list tile pressed");
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      // TODO: Instead of pointing to exactly 1 Category widget,
-      // our home should now point to an instance of the CategoryRoute widget.
-      home: CategoryRoute(),
+      home: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  child: Image.network(
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwpMa-LnTiy607Vq4zIcch4HU8JwnF6VCyyZWojL53UK9LobIzTA",),
+                ),
+                accountEmail: Text("a@a.com"),
+                accountName: Text("Xuzan"),
+              ),
+              getListTile("Home", Icons.home),
+              getListTile("Settings", Icons.settings),
+              getListTile("About us", Icons.account_box),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text(
+            "Test",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("test");
+          },
+          child: Icon(Icons.add),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text("This is list tile"),
+                subtitle: Text("This is subtitle"),
+                leading: Icon(Icons.add),
+                onTap: () {
+                  print("list tile pressed");
+                },
+              ),
+              getBox(Colors.red),
+              getBox(Colors.blue),
+              getBox(Colors.green),
+              getBox(Colors.black),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
